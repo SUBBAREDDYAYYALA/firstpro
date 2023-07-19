038d87e8af595f5c40525ca314abb255ac62e33a
SUBBAREDDYAYYALA <subbareddya2022@gmail.com>
2023-07-19 19:05:58 +0530

    first commit

---
+++ aug18_1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Aug181Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aug18_1'
---
+++ aug18_1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def nandha(request):
+    return HttpResponse('<i>lover boy nanadha........<i/>')
